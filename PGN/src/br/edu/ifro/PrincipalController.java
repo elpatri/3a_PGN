b8ec0b388d391bf04574f0939b4c28c01fa1d20d
Patrick Neris <patrick.g.neris@gmail.com>
2018-05-02 14:41:46 -0400

    Instalação da biblioteca JFoenix

--- PGN/src/br/edu/ifro/PrincipalController.java
+++ PGN/src/br/edu/ifro/PrincipalController.java
@@ -8,23 +8,64 @@ package br.edu.ifro;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 
 /**
  *
- * @author Patrick Galvão
+ * @author Patrick Neris
  */
 public class PrincipalController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField txt1;
+    private TextField txt2;
+    private TextField txtRes;
+    private Button btnMulti;
+    private Button btnDiv;
+    private Button btnSoma;
+    private Button btnSub;
+    private Button btnLimpar;
+    
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void limpar(ActionEvent event) {
+        txt1.setText("");
+        txt2.setText("");
+        txtRes.setText("");
+    }
+    
+    private void multi (ActionEvent event){
+        double n1 = (Double.parseDouble(txt1.getText()));
+        double n2 = (Double.parseDouble(txt2.getText()));
+        double res = n1*n2;
+        txtRes.setText(Double.toString(res));
+        
+    }
+    
+    private void div (ActionEvent event){
+        double n1 = (Double.parseDouble(txt1.getText()));
+        double n2 = (Double.parseDouble(txt2.getText()));
+        double res = n1/n2;
+        txtRes.setText(Double.toString(res));
+    }
+    private void soma (ActionEvent event){
+        double n1 = (Double.parseDouble(txt1.getText()));
+        double n2 = (Double.parseDouble(txt2.getText()));
+        double res = n1+n2;
+        txtRes.setText(Double.toString(res));
+    }
+    
+    private void sub (ActionEvent event){
+        double n1 = (Double.parseDouble(txt1.getText()));
+        double n2 = (Double.parseDouble(txt2.getText()));
+        double res = n1-n2;
+        txtRes.setText(Double.toString(res));
     }
     
     @Override
